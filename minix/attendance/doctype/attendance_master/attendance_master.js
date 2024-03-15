0acf1372b0dd871583037292eb4451ae856da6b0
SubodhBarche322 <subodhbarche@gmail.com>
2024-03-15 10:47:39 +0530

    First commit

---
+++ minix/attendance/doctype/attendance_master/attendance_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Minix and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Attendance Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ minix/attendance/doctype/attendance_master/test_attendance_master.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Minix and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAttendanceMaster(FrappeTestCase):
+	pass
